2c138bc1b86482fa735e6631dcbb19bfef3ae512
foxriver76 <moritz.heusinger@gmail.com>
2023-10-25 15:21:39 +0200

    improve test output

--- test/testSchema.ts
+++ test/testSchema.ts
@@ -2,7 +2,7 @@ import Ajv from "ajv"
 import fs from 'node:fs'
 import path from 'node:path'
 
-const ajv = new Ajv({ allErrors: true, allowUnionTypes: true, strictTypes: false })
+const ajv = new Ajv({ allErrors: true, strict: false })
 const basePath = path.join(__dirname, '..', 'schemas');
 
 const schema = fs.readFileSync(path.join(basePath, 'jsonConfig.json'), { encoding: 'utf-8' });
@@ -15,6 +15,9 @@ for (const fileName of ['testJsonConfig.json', 'testJSONConfigPanel.json']) {
     const valid = validate(config);
 
     if (!valid) {
-        throw new Error(JSON.stringify(validate.errors))
+        const errors = validate.errors!.map(entry => JSON.stringify(entry, null, 2))
+        console.error((errors.join('\n')))
+        console.error(`${errors.length} errors occurred on ${fileName}`)
+        process.exit(1)
     }
 }
