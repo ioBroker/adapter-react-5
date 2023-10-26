6d1db54f607b0ad4e0b561b31aae9d00029165e2
foxriver76 <moritz.heusinger@gmail.com>
2023-10-26 14:22:02 +0200

    fix tables for a few types
    - and also add failing tests to minimal tests

--- test/testSchema.ts
+++ test/testSchema.ts
@@ -2,22 +2,101 @@ import Ajv from "ajv"
 import fs from 'node:fs'
 import path from 'node:path'
 
-const ajv = new Ajv({ allErrors: true, strict: false })
+const ajv = new Ajv({allErrors: true, strict: false})
 const basePath = path.join(__dirname, '..', 'schemas');
 
-const schema = fs.readFileSync(path.join(basePath, 'jsonConfig.json'), { encoding: 'utf-8' });
+const schema = fs.readFileSync(path.join(basePath, 'jsonConfig.json'), {encoding: 'utf-8'});
 
 const validate = ajv.compile(JSON.parse(schema))
 
-for (const fileName of ['testJsonConfig.json', 'testJSONConfigPanel.json']) {
-    const content = fs.readFileSync(path.join(basePath, fileName), { encoding: 'utf-8' });
-    const config = JSON.parse(content);
-    const valid = validate(config);
+/**
+ * Tests which need to be passed
+ */
+function positiveTests(): void {
+    for (const fileName of ['testJsonConfig.json', 'testJSONConfigPanel.json']) {
+        const content = fs.readFileSync(path.join(basePath, fileName), {encoding: 'utf-8'});
+        const config = JSON.parse(content);
+        const valid = validate(config);
 
-    if (!valid) {
-        const errors = validate.errors!.map(entry => JSON.stringify(entry, null, 2))
-        console.error((errors.join('\n')))
-        console.error(`${errors.length} errors occurred on ${fileName}`)
-        process.exit(1)
+        if (!valid) {
+            const errors = validate.errors!.map(entry => JSON.stringify(entry, null, 2))
+            console.error((errors.join('\n')))
+            console.error(`${errors.length} errors occurred on ${fileName}`)
+            process.exit(1)
+        }
     }
 }
+
+/** Expected errors per test */
+const expectedErrorsPerTest = {
+    'testFailJsonConfig.json': [
+        {
+            "instancePath": "/items/demoTab/items/myTable/items/2",
+            "schemaPath": "#/patternProperties/%5E.%2B/allOf/20/then/properties/items/items/allOf/16/then/additionalProperties",
+            "keyword": "additionalProperties",
+            "params": {
+                "additionalProperty": "test"
+            },
+            "message": "must NOT have additional properties"
+        },
+        {
+            "instancePath": "/items/demoTab/items/myTable/items/2",
+            "schemaPath": "#/patternProperties/%5E.%2B/allOf/20/then/properties/items/items/allOf/16/if",
+            "keyword": "if",
+            "params": {
+                "failingKeyword": "then"
+            },
+            "message": "must match \"then\" schema"
+        },
+        {
+            "instancePath": "/items/demoTab/items/myTable",
+            "schemaPath": "#/patternProperties/%5E.%2B/allOf/20/if",
+            "keyword": "if",
+            "params": {
+                "failingKeyword": "then"
+            },
+            "message": "must match \"then\" schema"
+        },
+        {
+            "instancePath": "/items/demoTab",
+            "schemaPath": "#/properties/items/patternProperties/%5E.%2B/allOf/8/if",
+            "keyword": "if",
+            "params": {
+                "failingKeyword": "then"
+            },
+            "message": "must match \"then\" schema"
+        }
+    ]
+} as const;
+
+
+/**
+ * Tests which should be failed
+ */
+function failingTests(): void {
+    for (const fileName of ['testFailJsonConfig.json'] as const) {
+        const content = fs.readFileSync(path.join(basePath, fileName), {encoding: 'utf-8'});
+        const config = JSON.parse(content);
+        const valid = validate(config);
+
+        if (valid) {
+            console.error(`Schema validation was successful at ${fileName}, but a fail was expected`)
+            process.exit(1)
+        }
+
+        const expectedErrors = expectedErrorsPerTest[fileName]
+
+        if (JSON.stringify(expectedErrors) !== JSON.stringify(validate.errors)) {
+            console.error(`Got different errors than expected on file ${fileName}`)
+            console.error(`Expected ${JSON.stringify(expectedErrors)}`)
+            console.error(`Got ${JSON.stringify(validate.errors)}`)
+            process.exit(1)
+        }
+    }
+
+
+}
+
+positiveTests()
+failingTests()
+
