639f09993243f4f60230a843bd2cc50157cda7ea
GermanBluefox <dogafox@gmail.com>
2024-09-13 11:50:19 +0800

    Used new eslint-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,4 @@
-import config from '@iobroker/eslint-config';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import config, { reactConfig } from '@iobroker/eslint-config';
 
 // disable temporary the rule 'jsdoc/require-param' and enable 'jsdoc/require-jsdoc'
 config.forEach(rule => {
@@ -9,38 +7,5 @@ config.forEach(rule => {
         rule.rules['jsdoc/require-param'] = 'off';
     }
 });
-config.push({
-    rules: {
-        'class-methods-use-this': 'warn',
-    }
-})
-config.push({
-    plugins: {
-        react: reactPlugin,
-        'react-hooks': reactHooksPlugin,
-    },
-    rules: {
-        ...reactPlugin.configs.recommended.rules,
-        ...reactHooksPlugin.configs.recommended.rules,
-        'react/no-unused-class-component-methods': 'warn',
-        'react/prop-types': 'off',
-        '@typescript-eslint/restrict-template-expressions': 'off',
-        'react/no-is-mounted': 'off',
-    },
-    languageOptions: {
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
-            ecmaVersion: 12,
-            jsxPragma: 'React', // for @typescript/eslint-parser
-        }
-    },
-    settings: {
-        react: {
-            version: 'detect', // Automatically detect the React version
-        },
-    },
-});
 
-export default [...config];
+export default [...config, ...reactConfig];
